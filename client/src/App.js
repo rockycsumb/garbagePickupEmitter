eb71f8a7650664992565b41a689725d935c5e393
rockycsumb <rocky@solidrockdesigns.io>
2023-10-22 15:18:34 -0700

    files

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import GarbagePickupEmitter from './GarbagePickupEmitter'
+function App() {
+  return (
+    <div className="App">
+     <GarbagePickupEmitter />
+    </div>
+  );
+}
+
+export default App;
